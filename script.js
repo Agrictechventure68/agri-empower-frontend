b822ac7367a8e957aa4f4a9045219512d52aa189
Brivicstar <brivicstarschools@gmail.com>
2025-06-13 19:43:08 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Smart Farming Platform is live");
+console.log("Agri_Empower frontend script is alive");
+console.log("Agri_Empower script loaded");
+document.getElementById(nav-learn).addEventListener('click', 0 =>
+    {
+        document.getElementById('
+            main-content').innerHTML =
+            .
+            <h2>Learning Hub</h2>
+            <p>Access tutorials and farming courses here</p>
+            ;
+})
