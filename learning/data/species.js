1199df51b0bde4bbed0f080d43597a1ed3ec5780
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-02-05 19:15:20 +0100

    Create species.js

---
+++ learning/data/species.js
@@ -0,0 +1,47 @@
+// learning/data/species.js
+
+window.AGRI_SPECIES = {
+  crops: {
+    vegetables: [
+      "tomato",
+      "okra",
+      "ugu"
+    ],
+    food_crops: [
+      "rice",
+      "maize",
+      "cassava"
+    ],
+    cash_crops: [
+      "cocoa",
+      "oil_palm",
+      "cotton"
+    ],
+    medicinal_crops: [
+      "aloe_vera",
+      "ginger",
+      "turmeric"
+    ]
+  },
+
+livestock: {
+    poultry: [
+      "broiler",
+      "layer"
+    ],
+    aquaculture: [
+      "catfish",
+      "tilapia"
+    ],
+    land_animals: [
+      "goat",
+      "sheep",
+      "cattle",
+      "rabbit",
+      "snail"
+    ],
+    beekeeping: [
+      "honey_bee"
+    ]
+  }
+};
