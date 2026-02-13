94c2a8006f034cfe551a38822b9bed9dea6ff329
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-02-13 08:39:36 +0100

    Update layout.js

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,1 +1,22 @@
-layout 
+document.getElementById("header").innerHTML = `
+<header>
+  <h1>Agri_Empower</h1>
+  <nav>
+    <a href="/index.html">Home</a>
+    <a href="/learning/curriculum.html">Curriculum</a>
+    <a href="/modules/diagnostic/diagnostic.html">Diagnostics</a>
+    <a href="/modules/ecommerce/ecommerce.html">Marketplace</a>
+    <a href="/profile.html">Farmers</a>
+    <a href="/farmers.html">Registered</a>
+    <a href="/about.html">About</a>
+    <a href="/contact.html">Contact</a>
+  </nav>
+</header>
+`;
+
+document.getElementById("footer").innerHTML = `
+<footer>
+  <p>&copy; 2025 Agri_Empower | Bright Doro AgricTech Venture</p>
+  <p>Lagos, Nigeria | agriempower4dcentury@gmail.com</p>
+</footer>
+`;
