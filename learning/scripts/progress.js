859ea18dd5bfcde9667a18061f9b2f97ccc0c658
Bright Doro <brightdoroagrictechventure@gmail.com>
2026-03-12 01:19:34 +0100

    Update progress.js

--- learning/scripts/progress.js
+++ learning/scripts/progress.js
@@ -1,1 +1,17 @@
-progress 
+function saveProgress(key, value) {
+  localStorage.setItem(key, value);
+}
+
+function getProgress(key) {
+  return localStorage.getItem(key);
+}
+
+function markLessonComplete(enterprise, pillar, level) {
+
+  const progressKey = `${enterprise}_${pillar}_${level}`;
+
+  saveProgress(progressKey, "completed");
+
+  console.log("Progress saved:", progressKey);
+
+}
